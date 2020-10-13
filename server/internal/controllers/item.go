ea6bc812f05b406a679ff4f29192b8445b83d98a
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-13 16:19:02 -0600

    refactor: move item model to 'models' directory

--- server/internal/controllers/item.go
+++ server/internal/controllers/item.go
@@ -2,34 +2,28 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/veech/joshai-coding-quiz/internal/models"
 	"net/http"
 )
 
-// item uses an int to store currency as floats can be unpredictable
-type item struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Img         string `json:"img"`
-	Price       int    `json:"price"`
-}
-
 // ItemController contains the route handlers for "item" routes
 type ItemController struct {
-	itemList []item
+	itemList []models.Item
+}
+
+// NewItemController initilizes a new ItemController with the mock data
+func NewItemController() ItemController {
+	// Hard coding these for now but ideally would be stored in a DB
+	defaultItems := []models.Item{
+		models.Item{Name: "item1", Description: "This is item1", Price: 100},
+		models.Item{Name: "item2", Description: "This is item2", Price: 450},
+		models.Item{Name: "item3", Description: "This is item3", Price: 325},
+	}
+
+	return ItemController{itemList: defaultItems}
 }
 
 // GetItems returns the list of items in the ItemController
 func (ic *ItemController) GetItems(c *gin.Context) {
 	c.JSON(http.StatusOK, ic.itemList)
-}
-
-// NewItemController initilizes a new ItemController with the mock data
-func NewItemController() ItemController {
-	defaultItems := []item{
-		item{Name: "item1", Description: "This is item1", Price: 100},
-		item{Name: "item2", Description: "This is item2", Price: 450},
-		item{Name: "item3", Description: "This is item3", Price: 325},
-	}
-
-	return ItemController{itemList: defaultItems}
 }
---
+++ server/internal/models/item.go
@@ -0,0 +1,9 @@
+package models
+
+// Item uses an int to store currency as floats can be unpredictable
+type Item struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Img         string `json:"img"`
+	Price       int    `json:"price"`
+}
