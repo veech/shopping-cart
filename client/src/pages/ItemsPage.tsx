c6d117f97547ddbdc9c35b9dc3d46e79effeb209
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-14 11:20:32 -0600

    feat: basic items page

---
+++ client/src/pages/ItemsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {}
+
+export const ItemsPage: React.FC<Props> = () => (
+  <section className="section">
+    <div className="container">
+      <h3 className="title is-3">Items</h3>
+    </div>
+  </section>
+)
