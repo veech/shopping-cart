a1b55b9c701774f4e0a80ee9a83a8e99e79da2c8
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-14 16:29:58 -0600

    feat: initialize axios

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import { initializeAxios } from './utils/initialize-axios'
+
+initializeAxios()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
