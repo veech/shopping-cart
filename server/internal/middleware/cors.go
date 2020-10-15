52755057a16e61fd431513e3aafe4aa8f3c476e7
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-14 22:15:48 -0600

    fix: cors middleware not allowing token

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -9,7 +9,12 @@ func CORSMiddleware(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, token, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(200)
+		return
+	}
 
 	c.Next()
 }
