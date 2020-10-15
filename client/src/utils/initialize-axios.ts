e4514a0c50f234f45a30e960b943e1d1e505eb1a
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-14 22:16:30 -0600

    feat: send requests as user

--- client/src/utils/initialize-axios.ts
+++ client/src/utils/initialize-axios.ts
@@ -2,4 +2,6 @@ import axios from 'axios'
 
 export const initializeAxios = () => {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8080'
+  // Defaulting user to 'user1' for simplicity
+  axios.defaults.headers = { token: 'user1' }
 }
