464d5b9de722b512426b1bcb287bfc8bd4541d80
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-14 23:20:55 -0600

    feat: render toast container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { Nav } from './components/navigation/Nav'
 
 import { CartPage } from './pages/CartPage'
 import { ShopPage } from './pages/ShopPage'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 export const App = () => (
   <div className="app">
+    <ToastContainer position="bottom-right" newestOnTop />
     <Router>
       <Nav />
 
