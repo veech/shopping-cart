5841711760fc63a665a49b9dd8322a87dc76d531
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-15 08:32:09 -0600

    feat: shop page

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { Loader } from '../components/common/Loader'
+import { Card } from '../components/Card'
+
+import { ItemService } from '../services/ItemService'
+
+import { Item } from '../utils/types'
 
 interface Props {}
 
-export const ShopPage: React.FC<Props> = () => (
-  <section className="section">
-    <div className="container">
-      <h3 className="title is-3">Shop</h3>
+const renderItems = (items: Array<Item> | null) => {
+  if (!items) return <Loader />
+
+  const cards = items.map(item => (
+    <div key={item.id} className="column is-one-third">
+      <Card id={item.id} name={item.name} description={item.description} price={item.price} img={item.img} />
     </div>
-  </section>
-)
+  ))
+
+  return <div className="columns is-multiline">{cards}</div>
+}
+
+export const ShopPage: React.FC<Props> = () => {
+  const [items, setItems] = useState<Array<Item> | null>(null)
+
+  useEffect(() => {
+    ItemService.getAll().then(items => setItems(items))
+  }, [])
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h3 className="title is-3">Shop</h3>
+        {items ? <h5 className="subtitle is-5">{items?.length} items</h5> : null}
+
+        {renderItems(items)}
+      </div>
+    </section>
+  )
+}
