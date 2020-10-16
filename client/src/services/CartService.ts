f0eae722953a7325bd85d307c5741c4a3f637f7e
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-15 20:12:46 -0600

    feat: update cart item service

--- client/src/services/CartService.ts
+++ client/src/services/CartService.ts
@@ -8,13 +8,18 @@ interface NewCartItem {
 }
 
 export class CartService {
+  static async getAll(): Promise<Array<CartItem>> {
+    const { data } = await axios.get<Array<CartItem>>('/cart')
+    return data
+  }
+
   static async addItem(newCartItem: NewCartItem): Promise<CartItem> {
     const { data } = await axios.post<CartItem>('/cart', newCartItem)
     return data
   }
 
-  static async getAll(): Promise<Array<CartItem>> {
-    const { data } = await axios.get<Array<CartItem>>('/cart')
+  static async updateItemQuantity(itemId: string, quantity: number): Promise<CartItem> {
+    const { data } = await axios.patch<CartItem>(`/cart/${itemId}`, { quantity })
     return data
   }
 }
