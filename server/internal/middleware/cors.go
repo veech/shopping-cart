70cf9ba5b4bb7e4873dcb6905a8ff3b1755d7c6b
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-15 21:13:19 -0600

    fix: add DELETE method to cors

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -9,7 +9,7 @@ func CORSMiddleware(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, token, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
 
 	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(200)
