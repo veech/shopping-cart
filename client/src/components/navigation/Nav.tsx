9fac7f44405dd99e6ccb6cb3a381339bd0ca96d6
Alessandro Vecchi <avecchi.53@gmail.com>
2020-10-15 21:48:22 -0600

    fix: navbar design

--- client/src/components/navigation/Nav.tsx
+++ client/src/components/navigation/Nav.tsx
@@ -10,18 +10,25 @@ export const Nav: React.FC<Props> = () => (
     <div className="container">
       <div className="level is-mobile">
         <div className="level-left">
+          <div className="level-item pr-4">
+            <h3 className="is-size-3 has-weight-bold">FUDE</h3>
+          </div>
+
           <NavLink className="level-item navlink" to="/shop" activeClassName="is-active">
             <strong>Shop</strong>
+          </NavLink>
+
+          <NavLink className="level-item navlink" to="/shop">
+            <strong>About us</strong>
           </NavLink>
         </div>
 
         <div className="level-right">
           <div className="level-item">
-            <NavLink className="button" to="/cart" activeClassName="is-info">
+            <NavLink className="navlink" to="/cart" activeClassName="is-active">
               <span className="icon">
                 <i className="fas fa-shopping-cart"></i>
               </span>
-              <strong>Checkout</strong>
             </NavLink>
           </div>
         </div>
